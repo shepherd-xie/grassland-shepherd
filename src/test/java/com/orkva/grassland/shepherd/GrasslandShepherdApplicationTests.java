dacc6c0e9f9e8dc9e8f9ae409b1adf902870fd80
Shepherd Xie <headley.xie@gmail.com>
2019-11-13 14:46:39 +0800

    init: 初始化项目

---
+++ src/test/java/com/orkva/grassland/shepherd/GrasslandShepherdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orkva.grassland.shepherd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrasslandShepherdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
