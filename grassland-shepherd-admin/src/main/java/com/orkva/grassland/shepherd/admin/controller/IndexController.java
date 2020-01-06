9b16dc7ce3e5344e3296737400166b6d30452037
Shepherd Xie <shepherd.xie@orkva.com>
2020-01-06 22:29:01 +0800

    feat: 侧边菜单

--- grassland-shepherd-admin/src/main/java/com/orkva/grassland/shepherd/admin/controller/IndexController.java
+++ grassland-shepherd-admin/src/main/java/com/orkva/grassland/shepherd/admin/controller/IndexController.java
@@ -1,15 +1,22 @@
 package com.orkva.grassland.shepherd.admin.controller;
 
 import com.orkva.grassland.shepherd.security.annotation.CurrentUser;
+import com.orkva.grassland.shepherd.service.sys.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    
+    @Autowired
+    private MenuService menuService;
 
     @GetMapping(value = {"", "index"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("menus", menuService.list());
         return "index";
     }
 
